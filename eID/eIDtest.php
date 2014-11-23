ac5768e3df5c35c38aa488397d9d691913697d86
Jan-Erik Revsbech <janerik@moc.net>
2014-11-24 00:44:21 +0100

    [FEATURE] Add eID Script

---
+++ eID/eIDtest.php
@@ -0,0 +1,9 @@
+<?php
+
+header('Content-type: application/json');
+$data = array (
+	'var' => 'value'
+);
+echo json_encode($data);
+
+
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,3 +2,5 @@
 if (!defined ("TYPO3_MODE"))    die ("Access denied.");
 
 t3lib_extMgm::addPItoST43($_EXTKEY,"pi1/class.tx_mocbeer_pi1.php","_pi1","list_type",0);
+
+$TYPO3_CONF_VARS['FE']['eID_include']['beer'] = 'EXT:' . $_EXTKEY . '/eID/eIDtest.php';
