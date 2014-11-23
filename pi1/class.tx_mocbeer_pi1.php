affd81be6ef1d8800fb4c3665919705c1d969e49
Jan-Erik Revsbech <janerik@moc.net>
2014-11-23 23:28:29 +0100

    [FEATURE] Implement list and single view of a beer

--- pi1/class.tx_mocbeer_pi1.php
+++ pi1/class.tx_mocbeer_pi1.php
@@ -1,17 +1,48 @@
 <?php
 
 class tx_mocbeer_pi1 extends tslib_pibase {
-	var $prefixId = "tx_moccontacts_pi1";           // Same as class name
+	var $prefixId = "tx_mocbeer_pi1";           // Same as class name
 	var $scriptRelPath = "pi1/class.tx_mocbeer_pi1.php"; // Path to this script relative to the extension dir.
 	var $extKey = "moc_beer"; // The extension key.
+
+	public $pi_checkCHash = FALSE;
 
 	/**
 	 * @param string $content
 	 * @param array $conf
 	 */
 	public function main($content, $conf) {
-		return '<pre>' . print_r($conf, TRUE)  . '</pre>';
-		return 'I\'m rendered as a plugin!';
+		if (intval($this->piVars['beer']) == 0) {
+			return $this->listBeers();
+		}
+		return $this->showBeer(intval($this->piVars['beer']));
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function listBeers() {
+		/** @var \TYPO3\CMS\Core\Database\DatabaseConnection $db */
+		$db = $GLOBALS['TYPO3_DB'];
+		$content = '<ul>';
+		foreach ($db->exec_SELECTgetRows('*', 'tx_mocbeer_beer', 'NOT deleted') as $row) {
+			$content .= '<li> ' . $this->pi_linkTP($row['name'], array($this->prefixId . '[beer]' => $row['uid'])) . '</li>';
+		}
+		$content .= '</ul>';
+		return $content;
+	}
+
+	/**
+	 * @param integer $uid
+	 * @return string
+	 */
+	protected function showBeer($uid) {
+		/** @var \TYPO3\CMS\Core\Database\DatabaseConnection $db */
+		$db = $GLOBALS['TYPO3_DB'];
+		$row = $db->exec_SELECTgetSingleRow('*', 'tx_mocbeer_beer', 'uid=' . intval($uid));
+		$content = '<h1>' . $row['name'] . '</h1>';
+		$content .= '<p>ABV: ' . $row['alcohol_by_volume'] . '%</p>';
+		return $content;
 	}
 
 }
